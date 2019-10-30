fcc9c4759d2cead0e25c1bea83d6bdae516b9412
Kellen Mace <kellen.mace@gmail.com>
2019-10-29 21:20:32 -0400

    Break up draft entry submission code into several smaller, single-purpose methods

--- src/Mutations/SubmitDraftEntry.php
+++ src/Mutations/SubmitDraftEntry.php
@@ -70,6 +70,9 @@ class SubmitDraftEntry implements Hookable, Mutation {
             'entry' => [
 				'type'        => Entry::TYPE,
 				'description' => __( 'The entry that was created.', 'wp-graphql-gravity-forms' ),
+				'resolve' => function( array $payload ) : array {
+					return $this->entry_data_manipulator->manipulate( GFAPI::get_entry( $payload['entryId'] ) );
+				}
 			],
 		];
     }
@@ -86,39 +89,55 @@ class SubmitDraftEntry implements Hookable, Mutation {
             }
 
 			$resume_token = sanitize_text_field( $input['resumeToken'] );
-			$draft_entry  = GFFormsModel::get_draft_submission_values( $resume_token );
+			$draft_entry  = $this->get_draft_entry( $resume_token );
 
-			if ( ! is_array( $draft_entry ) || empty( $draft_entry['form_id'] ) ) {
-				throw new UserError( __( 'A draft with this resume token could not be found.', 'wp-graphql-gravity-forms' ) );
-			}
+			$this->validate_form_id( $draft_entry['form_id'] );
 
-            $form_info = GFFormsModel::get_form( $draft_entry['form_id'], true );
-
-            if ( ! $form_info || ! $form_info->is_active || $form_info->is_trash ) {
-                throw new UserError( __( 'The form associated with this entry is nonexistent or inactive.', 'wp-graphql-gravity-forms' ) );
-            }
-
-            $submission = json_decode( $draft_entry['submission'], true );
-
-            if ( ! $submission ) {
-                throw new UserError( __( 'The submission data for this draft entry could not be read.', 'wp-graphql-gravity-forms' ) );
-            }
-
-            $entry_id = GFAPI::add_entry( $submission['partial_entry'] );
-
-            if ( is_wp_error( $entry_id ) ) {
-                throw new UserError( __( 'An error occurred while trying to submit the draft entry.', 'wp-graphql-gravity-forms' ) . ' ' . $entry_id->get_error_message() );
-            }
-
-            $entry = $this->entry_data_manipulator->manipulate( GFAPI::get_entry( $entry_id ) );
+			$entry_id = $this->create_entry( $draft_entry );
 
             GFFormsModel::delete_draft_submission( $resume_token );
             GFFormsModel::purge_expired_draft_submissions();
 
-			return [
-                'entryId' => $entry_id,
-                'entry'   => $entry,
-			];
+			return [ 'entryId' => $entry_id ];
 		};
+	}
+
+	private function get_draft_entry( string $resume_token ) : array {
+		$draft_entry  = GFFormsModel::get_draft_submission_values( $resume_token );
+
+		if ( ! is_array( $draft_entry ) || empty( $draft_entry['form_id'] ) ) {
+			throw new UserError( __( 'A draft with this resume token could not be found.', 'wp-graphql-gravity-forms' ) );
+		}
+
+		return $draft_entry;
+	}
+
+	private function validate_form_id( int $form_id ) {
+		$form_info = GFFormsModel::get_form( $form_id, true );
+
+		if ( ! $form_info || ! $form_info->is_active || $form_info->is_trash ) {
+			throw new UserError( __( 'The form associated with this entry is nonexistent or inactive.', 'wp-graphql-gravity-forms' ) );
+		}
+	}
+
+	private function create_entry( array $draft_entry ) : int {
+		$submission = $this->get_draft_submission( $draft_entry );
+		$entry_id   = GFAPI::add_entry( $submission['partial_entry'] );
+
+		if ( is_wp_error( $entry_id ) ) {
+			throw new UserError( __( 'An error occurred while trying to submit the draft entry.', 'wp-graphql-gravity-forms' ) . ' ' . $entry_id->get_error_message() );
+		}
+
+		return $entry_id;
+	}
+
+	private function get_draft_submission( array $draft_entry ) : array {
+		$submission = json_decode( $draft_entry['submission'], true );
+
+		if ( ! $submission ) {
+			throw new UserError( __( 'The submission data for this draft entry could not be read.', 'wp-graphql-gravity-forms' ) );
+		}
+
+		return $submission;
 	}
 }
