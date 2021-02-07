231f293630c3419f7ec5f0fce063c017584bf2d4
justlevine <justlevine@gmail.com>
2021-02-07 08:51:00 +0200

    dump-autoload --no-dev --optimize

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,11 +13,16 @@ class ComposerAutoloaderInitb6a81c7a01d0c87dfabe35600fabda62
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
+
+        require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitb6a81c7a01d0c87dfabe35600fabda62', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
@@ -25,7 +30,7 @@ class ComposerAutoloaderInitb6a81c7a01d0c87dfabe35600fabda62
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitb6a81c7a01d0c87dfabe35600fabda62::getInitializer($loader));
         } else {
