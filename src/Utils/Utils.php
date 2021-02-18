a2dc5289a37c62b116f1b312d63c18301356cf96
justlevine <justlevine@gmail.com>
2021-02-18 03:28:36 +0200

    Add deprecate_property() function

---
+++ src/Utils/Utils.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Utils
+ *
+ * Common utility functions
+ *
+ * @package WPGraphQLGravityForms\Utils
+ * @since 0.1.0
+ */
+
+namespace WPGraphQLGravityForms\Utils;
+
+/**
+ * Class - Utils
+ */
+class Utils {
+
+	/**
+	 * Adds deprecation reason to GraphQL field property.
+	 *
+	 * @param array  $property The field property to deprecate.
+	 * @param string $reason The reason for the deprecation. Should be wrapped in __().
+	 *
+	 * @return array
+	 * @since 0.1.0
+	 */
+	public function depricate_property( array $property, string $reason ) : array {
+		$property_key = array_key_first( $property );
+
+		// Add deprecation reason to property.
+		if ( isset( $property_key ) ) {
+			$property[ $property_key ]['deprecationReason'] = $reason;
+		}
+
+		return $property;
+	}
+}
