c533d47981bd43f6a526cf98da04e494a25226b7
justlevine <justlevine@gmail.com>
2021-02-18 03:27:05 +0200

    Move nonglobal properties from get_global_properties()

--- src/Types/Field/Field.php
+++ src/Types/Field/Field.php
@@ -7,6 +7,7 @@
  *
  * @package WPGraphQLGravityForms\Types\Field
  * @since   0.0.1
+ * @since   0.1.0 Remove adminLabel, adminOnly, allowsPrepopulate, label, and visibility from global properties.
  */
 
 namespace WPGraphQLGravityForms\Types\Field;
@@ -14,7 +15,6 @@ namespace WPGraphQLGravityForms\Types\Field;
 use WPGraphQLGravityForms\Interfaces\Hookable;
 use WPGraphQLGravityForms\Interfaces\Type;
 use WPGraphQLGravityForms\Types\ConditionalLogic\ConditionalLogic;
-use WPGraphQLGravityForms\Types\Enum\FieldVisibilityEnum;
 
 /**
  * Class - Field
@@ -27,52 +27,33 @@ abstract class Field implements Hookable, Type {
 	 */
 	protected function get_global_properties() : array {
 		return [
-			'adminLabel'        => [
-				'type'        => 'String',
-				'description' => __( 'When specified, the value of this property will be used on the admin pages instead of the label. It is useful for fields with long labels.', 'wp-graphql-gravity-forms' ),
-			],
-			'adminOnly'         => [
-				'type'        => 'Boolean',
-				'description' => __( 'Determines if this field should only visible on the administration pages. A value of 1 will mark the field as admin only and will hide it from the public form. Useful for fields such as “status” that help with managing entries, but don’t apply to users filling out the form.', 'wp-graphql-gravity-forms' ),
-			],
-			'allowsPrepopulate' => [
-				'type'        => 'Boolean',
-				'description' => __( 'Determines if the field’s value can be pre-populated dynamically. 1 to allow field to be pre-populated, 0 otherwise.', 'wp-graphql-gravity-forms' ),
-			],
-			'conditionalLogic'  => [
+			'conditionalLogic' => [
 				'type'        => ConditionalLogic::TYPE,
 				'description' => __( 'Controls the visibility of the field based on values selected by the user.', 'wp-graphql-gravity-forms' ),
 			],
-			'cssClass'          => [
+			'cssClass'         => [
 				'type'        => 'String',
 				'description' => __( 'String containing the custom CSS classes to be added to the <li> tag that contains the field. Useful for applying custom formatting to specific fields.', 'wp-graphql-gravity-forms' ),
 			],
-			'cssClassList'      => [
+			'cssClassList'     => [
 				'type'        => [ 'list_of' => 'String' ],
 				'description' => __( 'Array of the custom CSS classes to be added to the <li> tag that contains the field. Useful for applying custom formatting to specific fields.', 'wp-graphql-gravity-forms' ),
 			],
-			'id'                => [
-				'type'        => 'Integer',
-				'description' => __( 'Field ID.', 'wp-graphql-gravity-forms' ),
-			],
-			'label'             => [
-				'type'        => 'String',
-				'description' => __( 'Field label that will be displayed on the form and on the admin pages.', 'wp-graphql-gravity-forms' ),
-			],
-			'type'              => [
-				'type'        => 'String',
-				'description' => __( 'The type of field to be displayed.', 'wp-graphql-gravity-forms' ),
-			],
-			'formId'            => [
+			'formId'           => [
 				'type'        => 'Integer',
 				'description' => __( 'The ID of the form this field belongs to.', 'wp-graphql-gravity-forms' ),
 			],
-			'visibility'        => [
+			'id'               => [
+				'type'        => 'Integer',
+				'description' => __( 'Field ID.', 'wp-graphql-gravity-forms' ),
+			],
+			'type'             => [
 				'type'        => 'String',
-				'description' => __( 'Field visibility. Possible values: visible, hidden, or administrative.', 'wp-graphql-gravity-forms' ),
+				'description' => __( 'The type of field to be displayed.', 'wp-graphql-gravity-forms' ),
 			],
 		];
 	}
+
 
 	/**
 	 * Get the custom properties.
