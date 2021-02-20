e59666ba789b9b7d997167664cbfffb3ecfcf1c8
justlevine <justlevine@gmail.com>
2021-02-20 16:45:44 +0200

    Bugfix: fix descriptionPlacement definition

--- src/Types/Field/FieldProperty/DescriptionPlacementProperty.php
+++ src/Types/Field/FieldProperty/DescriptionPlacementProperty.php
@@ -24,8 +24,7 @@ abstract class DescriptionPlacementProperty implements FieldProperty {
 	 */
 	public static function get() : array {
 		return [
-			// @TODO - Convert to enum. Possible values: "above" or "below"
-			'choices' => [
+			'descriptionPlacement' => [
 				'type'        => 'String',
 				'description' => __( 'The placement of the field description. The description may be placed “above” or “below” the field inputs. If the placement is not specified, then the description placement setting for the Form Layout is used.', 'wp-graphql-gravity-forms' ),
 			],
