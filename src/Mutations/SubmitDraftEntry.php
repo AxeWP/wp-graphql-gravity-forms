c6917f69514a08a9aeeb4be627e272d3b9c0de6f
justlevine <justlevine@gmail.com>
2021-03-03 00:47:07 +0200

    Add @since changelog

--- src/Mutations/SubmitDraftEntry.php
+++ src/Mutations/SubmitDraftEntry.php
@@ -6,6 +6,7 @@
  *
  * @package WPGraphQLGravityForms\Mutation
  * @since 0.0.1
+ * @since 0.3.0 Sets the form pageNumber to the last page, to avoid post creation validation issues.
  */
 
 namespace WPGraphQLGravityForms\Mutations;
