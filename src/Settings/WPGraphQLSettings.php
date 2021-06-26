ce7a333e79429c74e14608c7c7f3facd06c00461
Dovid Levine <justlevine@gmail.com>
2021-06-26 00:49:10 +0000

    fix: correctly set graphql_connection_max_query_amount

--- src/Settings/WPGraphQLSettings.php
+++ src/Settings/WPGraphQLSettings.php
@@ -8,8 +8,9 @@
 
 namespace WPGraphQLGravityForms\Settings;
 
+use GraphQL\Type\Definition\ResolveInfo;
+use WPGraphQL\AppContext;
 use WPGraphQLGravityForms\Interfaces\Hookable;
-
 /**
  * WPGraphQL Settings.
  */
@@ -22,18 +23,24 @@ class WPGraphQLSettings implements Hookable {
 	 * @see: https://www.wpgraphql.com/filters/graphql_connection_max_query_amount/
 	 */
 	public function register_hooks() : void {
-		add_action( 'graphql_connection_max_query_amount', [ $this, 'set_max_query_amount' ], 11 );
+		add_filter( 'graphql_connection_max_query_amount', [ $this, 'set_max_query_amount' ], 11, 5 );
 	}
 
 	/**
 	 * Bump max query amount to account for forms with many fields.
 	 *
-	 * @param  int $max_query_amount Max query amount.
+	 * @param int         $max_query_amount Max query amount.
+	 * @param mixed       $source     source passed down from the resolve tree.
+	 * @param array       $args       array of arguments input in the field as part of the GraphQL query.
+	 * @param AppContext  $context    Object containing app context that gets passed down the resolve tree.
+	 * @param ResolveInfo $info       Info about fields passed down the resolve tree.
 	 *
 	 * @return int Max query amount, possibly bumped.
 	 */
-	public function set_max_query_amount( $max_query_amount ) : int {
-		// Original max amount or 600 - whichever is higher.
-		return (int) max( $max_query_amount, 600 );
+	public function set_max_query_amount( int $max_query_amount, $source, array $args, AppContext $context, ResolveInfo $info ) : int {
+		if ( 'formFields' === $info->fieldName ) {
+			return (int) max( $max_query_amount, 600 );
+		}
+		return $max_query_amount;
 	}
 }
