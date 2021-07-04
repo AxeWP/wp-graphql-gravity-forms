338702dea52ea28479dff78a6a9b48c242e6f51e
Dovid Levine <justlevine@gmail.com>
2021-07-04 17:59:52 +0300

    Release/0.6.3 (#124)
    * chore: version bump
    
    * chore: Update changelog
    
    * chore: add Update URI header

--- wp-graphql-gravity-forms.php
+++ wp-graphql-gravity-forms.php
@@ -6,7 +6,8 @@
  * Description: Adds Gravity Forms functionality to the WPGraphQL schema.
  * Author: Harness Software
  * Author URI: https://www.harnessup.com
- * Version: 0.6.2.2
+ * Update URI: https://github.com/harness-software/wp-graphql-gravity-forms/releases
+ * Version: 0.6.3
  * Text Domain: wp-graphql-gravity-forms
  * Domain Path: /languages
  * Requires at least: 5.4.1
