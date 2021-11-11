d7e11162eb3bded4e4b9f4b293f08393488925ff
Dovid Levine <justlevine@gmail.com>
2021-11-11 13:17:26 +0000

    tests: add test for Utils::maybe_decode_json

--- tests/unit/UtilsUtilsTest.php
+++ tests/unit/UtilsUtilsTest.php
@@ -56,4 +56,31 @@ class UtilsUtilsTest extends \Codeception\Test\Unit {
 		$truncate = Utils::truncate( $string, 16 );
 		$this->assertEquals( $string, $truncate );
 	}
+
+	public function testMaybeDecodeJson(){
+		$value = [
+			'someKey' => 'someData'
+		];
+
+		$actual = Utils::maybe_decode_json( $value );
+		$this->assertEquals($value, $actual, 'Unable to pass array.' );
+
+		$expected = json_encode( $value);
+		$actual = Utils::maybe_decode_json( $expected );
+		$this->assertEquals($value, $actual, 'Unable to convert associative array.' );
+	
+		$value = [ 'someData', 'someOtherData' ];
+		$expected = json_encode( $value);
+		$actual = Utils::maybe_decode_json( $expected );
+		$this->assertEquals($value, $actual, 'Unable to convert non-associative array.' );
+
+		$expected = 235;
+		$actual = Utils::maybe_decode_json( $expected );
+		$this->assertFalse($actual, 'Unable to pass bad value' );
+
+
+		$value = 'some-string';
+		$actual = Utils::maybe_decode_json( $value );
+		$this->assertEquals( [$value], $actual, 'Unable to convert string to array' );
+	}
 }
