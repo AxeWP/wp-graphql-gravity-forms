45a43855a4a740d356c5791ff0d14e3d1bf2397c
Dovid Levine <justlevine@gmail.com>
2022-03-22 14:15:21 +0000

    chore: update min WPGraphQL version

--- wp-graphql-gravity-forms.php
+++ wp-graphql-gravity-forms.php
@@ -13,8 +13,8 @@
  * Requires at least: 5.4.1
  * Tested up to: 5.9.0
  * Requires PHP: 7.4+
- * WPGraphQL requires at least: 1.6.4+
- * GravityForms requires at least: 2.5.0+
+ * WPGraphQL requires at least: 1.7.0
+ * GravityForms requires at least: 2.5.0
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
