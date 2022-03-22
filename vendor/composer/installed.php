6787fedb1af0204812ba05d7001a3f2f979d8bd1
Dovid Levine <justlevine@gmail.com>
2022-03-22 17:54:23 +0000

    test pr

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '5ad32a26e742d1ef2e043bf7fcf44ac12db8c2c5',
+        'reference' => 'c726e1d5ba6684fe85f630f8a07c5bf6e7a6dcbe',
         'name' => 'harness-software/wp-graphql-gravity-forms',
         'dev' => false,
     ),
@@ -16,7 +16,7 @@
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '5ad32a26e742d1ef2e043bf7fcf44ac12db8c2c5',
+            'reference' => 'c726e1d5ba6684fe85f630f8a07c5bf6e7a6dcbe',
             'dev_requirement' => false,
         ),
         'yahnis-elsts/plugin-update-checker' => array(
