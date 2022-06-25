ed62d2170b0c77078e473ba828616158f21c8475
Dovid Levine <justlevine@gmail.com>
2022-06-25 17:13:33 +0000

    chore: fix stan and lint

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '3ca94b4ed0ed23817b4067ea308566c57a307934',
+        'reference' => '24e8e2c550070bc3e02c132e5b8924536b4746ef',
         'name' => 'harness-software/wp-graphql-gravity-forms',
         'dev' => false,
     ),
@@ -16,7 +16,7 @@
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '3ca94b4ed0ed23817b4067ea308566c57a307934',
+            'reference' => '24e8e2c550070bc3e02c132e5b8924536b4746ef',
             'dev_requirement' => false,
         ),
         'yahnis-elsts/plugin-update-checker' => array(
