aefa7ee644502901143f940876d8697ff6bd5f38
David Levine <justlevine@gmail.com>
2022-07-12 19:43:26 +0000

    chore: update composer deps

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '24e8e2c550070bc3e02c132e5b8924536b4746ef',
+        'reference' => '4cc13315a1bf2c91a5e5c4a682f9ea6dd1b8e2ba',
         'name' => 'harness-software/wp-graphql-gravity-forms',
         'dev' => false,
     ),
@@ -16,7 +16,7 @@
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '24e8e2c550070bc3e02c132e5b8924536b4746ef',
+            'reference' => '4cc13315a1bf2c91a5e5c4a682f9ea6dd1b8e2ba',
             'dev_requirement' => false,
         ),
         'yahnis-elsts/plugin-update-checker' => array(
