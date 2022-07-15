f68500e689272cbe4e6aa3b2a6d0477b361c029d
Dovid Levine <justlevine@gmail.com>
2022-07-15 17:34:02 +0300

    chore: add composer.lock to repo (#280)
    * chore: add composer.lock to repo
    
    * chore: use `composer install` over `update`.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '4cc13315a1bf2c91a5e5c4a682f9ea6dd1b8e2ba',
+        'reference' => '05da4cee446bd62a805caf1f316be43f674d97cd',
         'name' => 'harness-software/wp-graphql-gravity-forms',
         'dev' => false,
     ),
@@ -16,7 +16,7 @@
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '4cc13315a1bf2c91a5e5c4a682f9ea6dd1b8e2ba',
+            'reference' => '05da4cee446bd62a805caf1f316be43f674d97cd',
             'dev_requirement' => false,
         ),
         'yahnis-elsts/plugin-update-checker' => array(
