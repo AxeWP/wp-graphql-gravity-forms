d72376da9171ec76cf4f1873eaaf8ff4ff6bc792
David Levine <david@axepress.dev>
2022-08-17 15:55:29 +0000

    chore: update plugin-update-checker to v4.13

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '49a1299791c25c6fd83542c6fedacddd' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v4p11.php',
+    '7166494aeff09009178f278afd86c83f' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v4p13.php',
 );
