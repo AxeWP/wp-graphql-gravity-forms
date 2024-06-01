6e6004b1a6317c064491e442fca3478aa962471c
David Levine <david@axepress.dev>
2024-06-01 08:47:33 +0000

    tests: cleanup

--- tests/_data/config.php
+++ tests/_data/config.php
@@ -1,8 +1,6 @@
 <?php
 // We use GRAPHQL_DEBUG responses in our tests.
 define( 'GRAPHQL_DEBUG', true );
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
 
 // Use reCAPTCHA test keys: https://developers.google.com/recaptcha/docs/faq
 define( 'GF_RECAPTCHA_PUBLIC_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' );
