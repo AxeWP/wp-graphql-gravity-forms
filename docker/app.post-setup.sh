858451bd25c5dc88004c949a276dcce698e8bc22
Dovid Levine <david@axepress.dev>
2025-05-11 00:21:11 +0300

    ci: list

--- docker/app.post-setup.sh
+++ docker/app.post-setup.sh
@@ -68,6 +68,8 @@ wp plugin activate wp-graphql-gravity-forms --allow-root
 # Set pretty permalinks.
 wp rewrite structure '/%year%/%monthnum%/%postname%/' --allow-root
 
+wp plugin list --allow-root
+
 # Export the db for codeception to use
 wp db export "${DATA_DUMP_DIR}/dump.sql" --allow-root
 
@@ -76,6 +78,7 @@ if $(wp maintenance-mode is-active --allow-root); then
 	echo "Deactivating maintenance mode"
 	wp maintenance-mode deactivate --allow-root
 fi
+
 
 chmod 777 -R .
 chown -R $(id -u):$(id -g) .
