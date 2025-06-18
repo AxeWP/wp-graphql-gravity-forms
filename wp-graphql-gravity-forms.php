22d3e4ae9bc74d1de65daac6621faa4cc64b3772
Dovid Levine <david@axepress.dev>
2025-06-18 23:39:03 +0300

    chore: headers bump

--- wp-graphql-gravity-forms.php
+++ wp-graphql-gravity-forms.php
@@ -11,12 +11,12 @@
  * Text Domain: wp-graphql-gravity-forms
  * Domain Path: /languages
  * Requires at least: 6.0
- * Tested up to: 6.8
+ * Tested up to: 6.8.1
  * Requires PHP: 7.4
  * Requires Plugins: wp-graphql
  * Gravity Forms requires at least: 2.7.0
  * WPGraphQL requires at least: 1.26.0
- * WPGraphQL tested up to: 1.32.1
+ * WPGraphQL tested up to: 2.3.3
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
