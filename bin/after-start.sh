a85fbc38fdf0288735c334dc8065d8a7667826c5
Dovid Levine <david@axepress.dev>
2026-01-22 16:35:24 +0200

    ci: html coverage reports

--- bin/after-start.sh
+++ bin/after-start.sh
@@ -7,6 +7,9 @@
 wait
 
 # Install the pdo_mysql extension on the provided container
+# Arguments:
+#   $1 - Docker Container ID
+#   $2 - wp-env Environment Name
 install_pdo_mysql() {
 	CONTAINER_ID="$1"
 	ENV_NAME="$2"
@@ -30,6 +33,29 @@ install_pdo_mysql() {
 	echo "pdo_mysql Extension on $ENV_NAME: Installed."
 }
 
+# Install PCOV
+# Arguments:
+#   $1 - wp-env Environment Name
+install_pcov() {
+	ENV_NAME="$1"
+
+	if npm run wp-env run $ENV_NAME -- php -m | grep -q pcov; then
+		echo "pcov Extension on $ENV_NAME: Already installed."
+		return 0
+	fi
+
+	echo "Installing: pcov Extension on $ENV_NAME."
+	if ! npm run wp-env run $ENV_NAME -- sudo pecl install pcov > /dev/null 2>&1; then
+		echo "WARNING: pcov Extension on $ENV_NAME: Installation failed. This is expected on ephemeral containers." >&2
+		return 0
+	fi
+
+	npm run wp-env run $ENV_NAME -- bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
+	npm run wp-env run $ENV_NAME -- bash -- -c 'echo "pcov.enabled=1" | sudo tee -a /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
+
+	echo "pcov Extension on $ENV_NAME: Installed."
+}
+
 # Install pdo_mysql extension in the tests-cli environment
 CONTAINER_ID="$(docker ps | grep tests-cli  | awk '{print $1}')"
 if [[ -n "$CONTAINER_ID" ]]; then
@@ -37,11 +63,8 @@ if [[ -n "$CONTAINER_ID" ]]; then
 fi
 
 if [[ "$PCOV_ENABLED" == "1" ]]; then
-	echo "Installing and enabling pcov extension for code coverage."
-	npm run wp-env run tests-cli -- sudo pecl install pcov
-
-	npm run wp-env run tests-cli bash -- -c 'echo "extension=pcov" | sudo tee /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
-	npm run wp-env run tests-cli bash -- -c 'echo "pcov.enabled=1" | sudo tee -a /usr/local/etc/php/conf.d/99-pcov.ini > /dev/null'
+	# Install pcov extension in the tests-cli environment
+	install_pcov "tests-cli"
 fi
 
 # Dump clean test database
